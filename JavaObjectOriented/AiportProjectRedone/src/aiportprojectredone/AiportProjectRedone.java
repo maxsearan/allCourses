0236bb070e38ab653600748645930ab5204a2b64
Laurian Staicu <staicu4@gmail.com>
2016-11-29 10:57:49 -0500

    homework

---
+++ JavaObjectOriented/AiportProjectRedone/src/aiportprojectredone/AiportProjectRedone.java
@@ -0,0 +1,55 @@
+package aiportprojectredone;
+
+/* Laurian Staicu 1696177
+ * IPD 9 
+ */
+class Airport {
+    private String code;
+        String city;
+	private String city;
+        private double latitude;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+        double longitude;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+	private double longitude;
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
+
+public class AiportProjectRedone {
+
+      
+
+    public static void main(String[] args) {
+        
+    }
+
+}
--- JavaObjectOriented/AirportTravel/src/airporttravel/test2.java
+++
@@ -1,9 +0,0 @@
-package airporttravel;
-
-/**
- *Internet Programming and Development IPD9
- * @author Laurian Staicu ID 1696177
- */
-public class test2 {
-
-}
